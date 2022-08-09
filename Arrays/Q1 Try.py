d290d98919626d23cb6db2562465a61b84ecb2ac
RAKSHITHANANDA P <rakshithanandap@gmail.com>
2022-08-09 23:26:47 +0530

    Initial commit

---
+++ Arrays/Q1 Try.py
@@ -0,0 +1,18 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        index_list = []
+        for i in range(len(matrix)):
+            if 0 in matrix[i]:
+                for index,value in enumerate(matrix[i]):
+                    if 0 == matrix[i][index]:
+                        index_list.append(index)
+                for j in range(len(matrix[i])):
+                    matrix[i][j] = 0
+        for i in range(len(matrix)):
+            for k in index_list:
+                matrix[i][k] = 0
+    
+        
